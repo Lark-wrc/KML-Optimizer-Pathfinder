3ff04c81e394f7ead4da9d0c8c12e42e31e77815
Bob <seedor87@students.rowan.edu>
2016-05-05 18:27:27 -0500

    added the integration of user input via text to useful fields for fasade. Still need to debug permissions error when open local input files

--- UI.py
+++ UI.py
@@ -12,13 +12,14 @@ import StaticMapsConnections.ImageMerge as ImageMerge
 
 class myFrame(Frame):
 
-    ftypes = [('KMZ files', '*.kmz'), ('KML files', '*.kml')]
+    ftypes = [('KML files', '*.kml')]
+    itypes = [('All files', '*'), ('PNG files', '*.png')]
     text = "testing"
     root = Tkinter.Tk()
     root.withdraw()
 
     # fields for user input, stored along with their respective entries
-    fields = 'latitude', 'longitude', 'distance'
+    fields = 'latitude', 'longitude', 'distance', 'size'
     entries = []
 
     # switch to request user input on go button
@@ -54,7 +55,7 @@ class myFrame(Frame):
 
         fileMenu = Menu(menubar)
         fileMenu.add_command(label="Open", command=self.onOpen)
-        fileMenu.add_command(label="Save", command=self.saveFile)
+        fileMenu.add_command(label="Save", command=self.saveFileKML)
         menubar.add_cascade(label="File", menu=fileMenu)
 
         for field in self.fields:
@@ -85,12 +86,17 @@ class myFrame(Frame):
         for entry in self.entries:
             field = entry[0]
             text  = entry[1].get()
-            print('%s: "%s"' % (field, text))
-            self.txt.insert(END, '%s: "%s"' % (field, text))
+            print('%s: %s' % (field, text))
+            self.txt.insert(END, '%s: %s' % (field, text))
             self.txt.insert(END, "\n")
 
-        # if the user has not selected a file yet, request that they select one now
+        # class wide storage of necessary center of focus info
+        self.lat = self.entries[0][1]
+        self.lng = self.entries[1][1]
+        self.dist = self.entries[2][1]
+
         self.driver()
+        return
 
     def onQuit(self):
         """
@@ -122,10 +128,10 @@ class myFrame(Frame):
             self.txt.insert(END, message)
             self.txt.insert(END, "\n")
             self.haveReadFile = True
-            return file
-        return file
+            return path
+        return os.path.split(file) [0]
 
-    def saveFile(self):
+    def saveFileKML(self):
         """
         Author: Bob Seedorf
 
@@ -146,8 +152,31 @@ class myFrame(Frame):
             self.txt.insert(END, message)
             self.txt.insert(END, "\n")
             self.haveReadFile = True
-            return file
-        return file
+            return path
+        return os.path.split(file) [0]
+
+    def saveFileImg(self):
+        """
+        Author: Bob Seedorf
+
+        This method will write, at the request of the user, the image file after processing
+        :param :
+        """
+
+        file = tkFileDialog.asksaveasfilename(parent=self.root, filetypes=self.itypes, title="Save the file as...",
+                                              defaultextension=".kml")
+        if file:
+            self.outimg = open(file, 'w')
+
+            path = os.path.split(file)[0]
+            name = os.path.split(file)[1]
+            pathname = path + "/" + name
+            message = "Successfully stored image in " + pathname
+            print message
+            self.txt.insert(END, message)
+            self.txt.insert(END, "\n")
+            return path
+        return os.path.split(file) [0]
 
     def driver(self):
         """
@@ -157,11 +186,8 @@ class myFrame(Frame):
         :param arg: file to be used as read in
         :return:
         """
-        # argone = 'Inputs\KML Files\us_states.kml'
 
-        # Create the KmlFasade
-
-        # fasade = KmlFasade(argarctic)
+        # Create the KmlFasade, force user input if not read file has been selected
         if(not self.haveReadFile):
             fasade = KmlFasade(self.onOpen())
         else:
@@ -169,20 +195,18 @@ class myFrame(Frame):
         fasade.placemarkToGeometrics()
         fasade.garbageFilter()
         f = RestrictionFactory()
-        f = f.newSquareRestriction([-103.528629, 41.260352], 500)
-        # f = f.newSquareRestriction([64.871826, 66.833851], 250)
+        f = f.newSquareRestriction([float(self.lng), float(self.lat)], float(self.size))
         f.restrict(fasade.geometrics)
         fasade.fasadeUpdate()
         if (not self.haveWriteFile):
-            fasade.rewrite(self.saveFile())
+            fasade.rewrite(self.saveFileKML())
         else:
             fasade.rewrite(self.outfile)
 
         # Build the Url
         build = UrlBuilder(600)
-
-        # build.centerparams('64.871826,66.833851', '4')
-        build.centerparams('41.260352,-103.528629', '4')
+        # build.centerparams('41.260352,-103.528629', '4')
+        build.centerparams('%s, %s' % (self.lat, self.lng), '%s' % (self.dist))
 
         markerlist = []
         for element in fasade.geometrics:
@@ -208,7 +232,8 @@ class myFrame(Frame):
             images = build.download()
             print "Downloaded."
             images = ImageMerge.convertPtoRGB(*images)
-            ImageMerge.mergeModeRGB('Outputs\Outfile.png', *images)
+            outimage = self.saveFileImg()
+            ImageMerge.mergeModeRGB(outimage, *images)
 
             # merges by downloading, merging, and repeating till none are left.
 
@@ -219,8 +244,7 @@ class myFrame(Frame):
             #     im = ImageMerge.convertPtoRGB(img)[0]
             #     layers.add(im)
 
-
-            im = Image.open('Outputs\Outfile.png')
+            im = Image.open(outimage)
             im.show()
 
 def main():
