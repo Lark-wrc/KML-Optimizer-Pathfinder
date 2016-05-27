21428ac02627ed517ffdc946b4ec44b9c7d0f399
Bob <seedor87@students.rowan.edu>
2016-05-27 16:13:47 -0400

    fixed more potentially broken paths

--- Drivers/Driver.py
+++ Drivers/Driver.py
@@ -1,10 +1,11 @@
 from GeometricDataStructures.KmlFasade import KmlFasade
+import os
 
-argzero = 'C:\Users\Bob S\PycharmProjects\KML-Optimizer-Pathfinder\Inputs\KML Files\\advancedexample1.kml'
-argtwo = 'Inputs\KML Files\\advancedexample2.kml'
-argone = 'C:\Users\Bob s\PycharmProjects\KML-Optimizer-Pathfinder\Inputs\KML Files\us_states.kml'
-argarctic = 'Inputs\KML Files\\arctic lines jan 31.kml'
-argtest = 'Inputs\KML Files\\tester.kml'
+argzero = os.path.dirname(os.path.dirname(__file__)) + '\Inputs\KML Files\\advancedexample1.kml'
+argtwo = os.path.dirname(os.path.dirname(__file__)) + '\Inputs\KML Files\\advancedexample2.kml'
+argone = os.path.dirname(os.path.dirname(__file__)) + '\Inputs\KML Files\us_states.kml'
+argarctic = os.path.dirname(os.path.dirname(__file__)) + '\Inputs\KML Files\\arctic lines jan 31.kml'
+argtest = os.path.dirname(os.path.dirname(__file__)) + '\Inputs\KML Files\\tester.kml'
 
 
 #Create the KmlFasade
