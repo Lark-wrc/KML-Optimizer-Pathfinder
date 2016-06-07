735da3eb094eba1b996d1f6f66ad962b37ed2cc1
lapost48 <lapost48@gmail.com>
2016-06-07 18:41:36 -0400

    Damn, capitals

--- Observations/__init__.py
+++ Observations/__init__.py
@@ -1,6 +1,6 @@
 from ObservableConsole import ObservableConsole
 from Observer import *
-from uiObserver import UiObserver
+from UiObserver import UiObserver
 from WaitObserver import WaitObserver
 
 __all__ = ['ObservableConsole', 'Observer', 'Observable', 'WaitObserver', 'UiObserver']
